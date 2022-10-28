00c5218f7ed9f0c3b4d3e193d822323c1e7c012b
CherryVoue <tskritz@gmail.com>
2022-10-28 15:32:29 -0400

    added comments

--- passwordcheck.py
+++ passwordcheck.py
@@ -1,11 +1,13 @@
+#  Imported function hides password input
 from getpass import getpass
 
 def strength_check(password):
+    #  Establish default conditions and special character requirements
     upper = 0
     lower = 0
     nums = 0
     specialChars = ["#","!","$","_"]
-
+    #  Verifies length, then presence of special characters, then numbers and uppercase/lowercase letters
     if len(password) >= 9:
         if any(char in password for char in specialChars):
             for char in password:
@@ -15,9 +17,10 @@ def strength_check(password):
                         upper+=1
                 elif char.islower():
                         lower+=1
+    #  Returns boolean, checking if the password meets the defined requirements through previous variables
     return (upper > 0 and lower > 0 and nums > 0)
                 
-    
+#  main function: runs until the user-defined password meets all requirements
 def main():
     password = ""
     while not strength_check(password):
@@ -25,5 +28,6 @@ def main():
         password = getpass()
     print("Password strength is sufficient")
 
+#  Runtime control, used to separate main() and strength_check() for use in a separate file
 if __name__ == "__main__":
     main()
